5459d7238eb44fef2d74cf1ed3db8d4d1858c3da
monja199 <monja.sesame@gmail.com>
2023-02-23 22:53:39 +0300

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+import app.urls as app_urls
+
+urlpatterns = [
+    path('', include(app_urls)),
+    path('admin/', admin.site.urls),
+]
+
